39d0ae22cd79d56d8c95cb23e0d0764db31ab21d
Ramon Noleto <devnoleto@gmail.com>
2025-06-26 21:04:43 -0300

    :tada: Solicitando permissões de acesso a Camera, Microfone e Album

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect, useRef, use } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Camera } from 'expo-camera';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { shareAsync } from 'expo-sharing';
+import * as MediaLibrary from 'expo-media-library';
+
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const [hasMicrophonePermission, setHasMicrophonePermission] = useState(false);
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState(false);
+
+  useEffect(()=> {
+    (async() => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microphonePermission = await Camera.requestMicrophonePermissionsAsync();
+      const mediaLibraryPermission =  await MediaLibrary.requestPermissionsAsync();
+
+      setHasCameraPermission(cameraPermission.status === 'granted');
+      setHasMicrophonePermission(microphonePermission.status === 'granted');
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === 'granted');
+
+    })()
+  },[]);
+
+  if(hasCameraPermission === undefined || hasMicrophonePermission === undefined){
+    return <Text>Não tem permissão de camera ou audio</Text>
+  }
+
+  if(hasMediaLibraryPermission === false){
+    return <Text>Não tem permissão de biblioteca de mídia</Text>
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
